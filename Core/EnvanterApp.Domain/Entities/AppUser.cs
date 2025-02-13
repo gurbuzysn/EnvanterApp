caf568a41a4f2d07cbd7a1dfe1c04ad27e2f012f
Yasin Gürbüz <yasingurbuz1@hotmail.com>
2025-02-13 07:59:03 +0300

    AppUser

---
+++ Core/EnvanterApp.Domain/Entities/AppUser.cs
@@ -0,0 +1,21 @@
+﻿using EnvanterApp.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnvanterApp.Domain.Entities
+{
+    public class AppUser
+    {
+        public Guid Id { get; set; }
+        public Status Status { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public Guid CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public Guid ModifiedBy { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public Guid DeletedBy { get; set; }
+    }
+}
