982b64b39acef70f689f1cb637ae77ee1d08653a
Yasin Gürbüz <yasingurbuz1@hotmail.com>
2025-03-16 11:51:56 +0300

    ...

--- Presentation/EnvanterApp.WebAPI/Program.cs
+++ Presentation/EnvanterApp.WebAPI/Program.cs
@@ -5,9 +5,13 @@ namespace EnvanterApp.WebAPI
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
             builder.Services.AddControllers();
+
             builder.Services.AddEndpointsApiExplorer();
+
             builder.Services.AddSwaggerGen();
+
             var app = builder.Build();
 
             if (app.Environment.IsDevelopment())
@@ -16,8 +20,11 @@ namespace EnvanterApp.WebAPI
                 app.UseSwaggerUI();
             }
             app.UseHttpsRedirection();
+
             app.UseAuthorization();
+
             app.MapControllers();
+
             app.Run();
         }
     }
