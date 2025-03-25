4f23df636b83f0c01548591a17776a296bf6d1b2
Yasin Gürbüz <yasingurbuz1@hotmail.com>
2025-03-25 18:40:08 +0300

    GeneralResponse

---
+++ Core/EnvanterApp.Application/Features/GeneralResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnvanterApp.Application.Features
+{
+    public class GeneralResponse
+    {
+        public object? Result { get; set; }
+        public string? Message { get; set; }
+        public bool IsSuccess { get; set; }
+        public HttpStatusCode Status { get; set; }
+    }
+}
