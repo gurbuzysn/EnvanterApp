e841fd305f0d1766f1d48745827ab5c3fa4d8af1
Yasin Gürbüz <yasingurbuz1@hotmail.com>
2025-04-14 23:43:35 +0300

    ...

---
+++ Infrastructure/EnvanterApp.Infrastructure/Services/Token/TokenHandler.cs
@@ -0,0 +1,22 @@
+﻿using EnvanterApp.Application.Abstractions.Token;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnvanterApp.Infrastructure.Services.Token
+{
+    public class TokenHandler : ITokenHandler
+    {
+        public Application.DTOs.Token CreateAccessToken()
+        {
+            Application.DTOs.Token token = new();
+
+
+        }
+    }
+}
+ 
