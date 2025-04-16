bf54845db595e2e4bc33462c5d192385cb544e75
Yasin Gürbüz <yasingurbuz1@hotmail.com>
2025-04-16 06:52:54 +0300

    TokenHandler

--- Infrastructure/EnvanterApp.Infrastructure/Services/Token/TokenHandler.cs
+++ Infrastructure/EnvanterApp.Infrastructure/Services/Token/TokenHandler.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,10 +12,31 @@ namespace EnvanterApp.Infrastructure.Services.Token
 {
     public class TokenHandler : ITokenHandler
     {
+        private readonly IConfiguration _configuration;
+
+        public TokenHandler(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         public Application.DTOs.Token CreateAccessToken()
         {
             Application.DTOs.Token token = new();
 
+
+            SymmetricSecurityKey securityKey = new(Encoding.UTF8.GetBytes(_configuration["Token:SecurityKey"]));
+
+
+
+            JwtSecurityToken securityToken = new JwtSecurityToken(
+                issuer: _configuration["Token:Issuer"],
+                audience: _configuration["Token:Audience"],
+                claims: null,
+                expires: token.Expiration,
+                notBefore: DateTime.UtcNow,
+                signingCredentials:
+                );
+
+            
 
         }
     }
