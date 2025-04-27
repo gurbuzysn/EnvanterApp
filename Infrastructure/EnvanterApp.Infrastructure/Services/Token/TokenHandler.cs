d6e427306300ead1130e1e07765a01401744baa3
Yasin Gürbüz <yasingurbuz1@hotmail.com>
2025-04-27 22:00:44 +0300

    ...

--- Infrastructure/EnvanterApp.Infrastructure/Services/Token/TokenHandler.cs
+++ Infrastructure/EnvanterApp.Infrastructure/Services/Token/TokenHandler.cs
@@ -19,8 +19,9 @@ namespace EnvanterApp.Infrastructure.Services.Token
             _configuration = configuration;
         }
         public Application.DTOs.Token CreateAccessToken()
-        {
-            var expiration = DateTime.UtcNow.AddMinutes(15);
+        { 
+            //var expiration = DateTime.UtcNow.AddMinutes(15);  UtcNow yapılacak
+            var expiration = DateTime.Now.AddMinutes(15);
             Application.DTOs.Token token = new()
             {
                 Expiration = expiration
@@ -30,18 +31,18 @@ namespace EnvanterApp.Infrastructure.Services.Token
             SigningCredentials signingCredentials = new(securityKey, SecurityAlgorithms.HmacSha256);
 
             JwtSecurityToken securityToken = new JwtSecurityToken(
-                issuer: _configuration["Token:Issuer"],
-                audience: _configuration["Token:Audience"],
-                claims: null,
-                expires: token.Expiration,
-                notBefore: DateTime.Now,  //DateTime.UtcNow yapılacak.
-                signingCredentials: signingCredentials
-                );
+                       issuer: _configuration["Token:Issuer"],
+                       audience: _configuration["Token:Audience"],
+                       claims: null,
+                       expires: token.Expiration,
+                       notBefore: DateTime.Now,  //DateTime.UtcNow yapılacak.
+                       signingCredentials: signingCredentials
+                       );
 
             JwtSecurityTokenHandler tokenHandler = new();
             token.AccessToken = tokenHandler.WriteToken(securityToken);
+
             return token;
         }
     }
 }
- 
