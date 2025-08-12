61d315cb73f80a07c5814a6b3260a745d406c443
Yasin Gürbüz <yasingurbuz1@hotmail.com>
2025-08-12 07:38:34 +0300

    Employee class changed

--- Core/EnvanterApp.Domain/Entities/Identity/Employee.cs
+++ Core/EnvanterApp.Domain/Entities/Identity/Employee.cs
@@ -9,9 +9,6 @@ namespace EnvanterApp.Domain.Entities.Identity
 {
     public class Employee : AppUser
     {
-        public List<Computer> Computers { get; set; } = new List<Computer>();
-        public List<Display> Displays { get; set; } = new List<Display>();
-        public List<Keyboard> Keyboards { get; set; } = new List<Keyboard>();
-        public List<Mouse> Mouses { get; set; } = new List<Mouse>();
+        
     }
 }
