8ca01de9b57219ec9941b402e3a5e7d58126a284
Yasin Gürbüz <yasingurbuz1@hotmail.com>
2025-10-02 07:18:27 +0300

    General Response - StatusCode

--- Core/EnvanterApp.Application/DTOs/GeneralResponse.cs
+++ Core/EnvanterApp.Application/DTOs/GeneralResponse.cs
@@ -7,5 +7,6 @@ namespace EnvanterApp.Application.DTOs
         public bool IsSuccess { get; set; }
         public string? Message { get; set; }
         public T? Result { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
     }
 }
